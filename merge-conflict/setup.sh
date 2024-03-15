cc2a9949c1ec3fb24be03f2066a2e7f47ce9cb19
Cristian Firmino <cmendesfirmino@gmail.com>
2024-03-14 21:21:02 -0300

    alterado merge conflict

--- merge-conflict/setup.sh
+++ merge-conflict/setup.sh
@@ -9,18 +9,17 @@ make-exercise-repo
 touch greeting.txt
 git add greeting.txt
 git commit -m "Add file greeting.txt"
-echo "hello" > greeting.txt
+echo "hello" >greeting.txt
 git add greeting.txt
 git commit -m "Add content to greeting.txt"
 
 # Create a file on branch1
 git checkout -b $kata-branch1
-echo "This is a relevant fact" > file.txt
+echo "This is a relevant fact" >file.txt
 git add file.txt
 git commit -m "add relevant fact"
 
-
-git checkout master
-echo "This is an indispensable truth!" > file.txt
+git checkout main
+echo "This is an indispensable truth!" >file.txt
 git add file.txt
 git commit -m "add indispensable truth!"
